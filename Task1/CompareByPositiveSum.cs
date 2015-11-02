7236d4daf9042ac992a1c33e451254c9a37732e9
stas <stanislav.berezovsky.95@gmail.com>
2015-11-02 23:00:25 +0200

    add all

---
+++ Task1/CompareByPositiveSum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task1
+{
+    public class CompareByPositiveSum:IArrayCompare
+    {
+        public int Compare(int[] a, int[] b)
+        {
+            if (a == null && b == null)
+                return 0;
+            if (a == null)
+                return 1;
+            if (b == null)
+                return -1;
+            int aSum = a.Sum();
+            int bSum = b.Sum();           
+            if (aSum > bSum)
+                return 1;
+            if (aSum ==bSum)
+                return 0;
+            return -1;
+            
+        }
+
+    }
+
+
+}
---
+++ Task1Test/JaggedArraySortTest.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using Task1;
+//using NUnit.Framework;
+
+namespace Task1Test
+{
+    [TestFixture]
+    public class JaggedArraySortTest
+    {
+        
+        [Test]
+        public void SortByPositiveSumTest()
+        {
+            int[][] array = { null,new[] { 3, -1, 5, -21, 31 }, new[] { 0, 24, -4, 10 },null,null, new[] { 5, 5, -10, 20 },null ,new[] { 7, -4, -3, 25, 15 } };
+            int[][] arrayCompareIsPositive = { new[] { 3, -1, 5, -21, 31 }, new[] { 5, 5, -10, 20 }, new[] { 0, 24, -4, 10 }, new[] { 7, -4, -3, 25, 15 },null,null,null,null };           
+            IArrayCompare sumPositiveCompare = new CompareByPositiveSum();
+            JaggedArraySort.Sort(array,sumPositiveCompare);
+            IStructuralEquatable arraySort = array;
+            Assert.AreEqual(arraySort.Equals(arrayCompareIsPositive , StructuralComparisons.StructuralEqualityComparer), true);
+        }
+        [Test]
+        public void SortByNegativeSumTest()
+        {
+            int[][] array = { null, new[] { 3, -1, 5, -21, 31 }, new[] { 0, 24, -4, 10 }, null, null, new[] { 5, 5, -10, 20 }, null, new[] { 7, -4, -3, 25, 15 } };
+            int[][] arrayCompareIsNegative = { new[] { 7, -4, -3, 25, 15 }, new[] { 0, 24, -4, 10 }, new[] { 5, 5, -10, 20 }, new[] { 3, -1, 5, -21, 31 }, null, null, null, null };
+            IArrayCompare sumNegativeCompare = new CompareByNegativeSum();
+            JaggedArraySort.Sort(array, sumNegativeCompare);
+            IStructuralEquatable arraySort = array;
+            Assert.AreEqual(arraySort.Equals(arrayCompareIsNegative, StructuralComparisons.StructuralEqualityComparer), true);
+
+        }
+
+        [Test]
+        public void SortByPositiveMaxElementTest()
+        {
+            int[][] array = { null, new[] { 3, -1, 5, -21, 31 }, new[] { 0, 24, -4, 10 }, null, null, new[] { 5, 5, -10, 20 }, null, new[] { 7, -4, -3, 25, 15 } };
+            int[][] arrayCompareIsPositive = { new[] { 5, 5, -10, 20 }, new[] { 0, 24, -4, 10 }, new[] { 7, -4, -3, 25, 15 },new[] { 3, -1, 5, -21, 31 }, null, null, null, null };
+            
+            IArrayCompare maxElementCompare = new CompareByPositiveMaxElement();
+            JaggedArraySort.Sort(array, maxElementCompare);
+            IStructuralEquatable arraySort = array;
+            Assert.AreEqual(arraySort.Equals(arrayCompareIsPositive, StructuralComparisons.StructuralEqualityComparer), true);
+        }
+
+        [Test]
+        public void SortByNegativeMaxElementTest()
+        {
+            int[][] array = { null, new[] { 3, -1, 5, -21, 31 }, new[] { 0, 24, -4, 10 }, null, null, new[] { 5, 5, -10, 20 }, null, new[] { 7, -4, -3, 25, 15 } };
+            int[][] arrayCompareIsNegative = { new[] { 3, -1, 5, -21, 31 }, new[] { 7, -4, -3, 25, 15 }, new[] { 0, 24, -4, 10 }, new[] { 5, 5, -10, 20 }, null, null, null, null };
+            IArrayCompare maxElementCompare = new CompareByNegativeMaxElement();
+            JaggedArraySort.Sort(array, maxElementCompare);
+            IStructuralEquatable arraySort = array;
+            Assert.AreEqual(arraySort.Equals(arrayCompareIsNegative, StructuralComparisons.StructuralEqualityComparer), true);
+
+        }
+    }
+}
